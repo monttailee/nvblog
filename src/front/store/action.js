96ad1e5085005df46c2ff8196356c73242255b22
monttailee <monttailee@163.com>
2017-11-30 14:39:57 +0800

    添加store文件,js文件

--- src/front/store/action.js
+++ src/front/store/action.js
@@ -6,14 +6,15 @@ import { GET_ALL_POSTS, GET_ALL_TAGS, GET_POST } from './mutationTypes'
 export default {
     async getAllPosts({ commit, state }, { tag = '', page = 1, limit = 5 } = {}){
         let res = await getAllPublishArticles(tag, page, limit);
-        commit(GET_ALL_POSTS, res)
+        commit(GET_ALL_POSTS, { posts: res.data.articleArr, allPage: res.data.allPage, curPage: page })
     },
     async getAllTags({ commit, state }){
         let res = await getAllTags();
-        commit(GET_ALL_TAGS, res)
+        commit(GET_ALL_TAGS, res.data.tagArr)
     },
     async getPost({ commit, state }, id){
 
+        //find() 方法返回数组中满足提供的测试函数的第一个元素的值
         let article = state.posts.find((post) => post.id == id);
         if (!article && state.currentPost.id == id) {
             article = state.currentPost;
@@ -24,7 +25,7 @@ export default {
 
         } else {
             let res = await getArticle(id);
-            commit(GET_POST, res)
+            commit(GET_POST, res.data.article)
         }
     }
 }
--- src/front/store/mutations.js
+++ src/front/store/mutations.js
@@ -8,6 +8,48 @@ import {
     GET_POST
 } from './mutationTypes'
 
-export default {
+import marked from '../assets/js/lib/marked'
 
+export default {
+    [GET_ALL_POSTS](state, { posts, allPage, curPage }){
+        if (isNaN(+allPage)) {
+            allPage = 0
+        }
+        if (isNaN(+curPage)) {
+            curPage = 0
+        }
+        state.posts = posts;
+        state.allPage = +allPage;
+        state.curPage = +curPage;
+    },
+    [GET_ALL_TAGS](state, tags){
+        state.tags = tags;
+    },
+    [SET_SELECT_TAGS](state, tags){
+        state.selectTags = tags;
+    },
+    [TOGGLE_SELECT_TAGS](state, { id, name }){
+        if (typeof state.selectTags.find(function(e) {
+                return e.id == id
+            }) == 'undefined') {
+            state.selectTags.push({
+                id,
+                name
+            })
+        } else {
+            state.selectTags = state.selectTags.filter((e) => {
+                return e.id !== id;
+            })
+        }
+    },
+    [TOGGLE_SIDEBOX](state){
+        state.sideBoxOpen = !state.sideBoxOpen;
+    },
+    [CLOSE_SIDEBOX](state){
+        state.sideBoxOpen = false;
+    },
+    [GET_POST](state, article){
+        state.currentPost = article;
+        state.currentPostCompile = marked(state.currentPost.content);
+    },
 }
